67cb156f9586df9450cb7706cc4e493ae1f2ff5d
jalal-sagaie <jalal.sagaie@gmail.com>
2024-11-10 11:39:38 +0100

    Init Projects

---
+++ ProjectManagment.Business/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ProjectManagment.Business
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ ProjectManagment.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ProjectManagment.Domain
+{
+    public class Class1
+    {
+
+    }
+}
