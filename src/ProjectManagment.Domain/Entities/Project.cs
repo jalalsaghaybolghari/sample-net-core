51a1b074054bd13f4bfad144f12b4214ea512864
jalal-sagaie <jalal.sagaie@gmail.com>
2024-11-10 11:55:34 +0100

    Coded Entities

---
+++ src/ProjectManagment.Domain/Entities/Project.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectManagment.Domain.Entities;
+
+public class Project : BaseEntity
+{
+    public string ProjectName { get; set; } 
+    public int? ProjectVersion { get; set; }
+    public DateTime StartDate { get; set; }
+}
