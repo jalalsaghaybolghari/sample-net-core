651d1d354c5a25fd7d4f6ca3e6e5fb00c706bf24
jalal-sagaie <jalal.sagaie@gmail.com>
2024-11-10 12:39:51 +0100

    Implemented DbContext to project

---
+++ src/ProjectManagment.Domain/DbContext/ProjectDbContext.cs
@@ -0,0 +1,24 @@
+﻿namespace ProjectManagment.Domain.DbContext;
+
+using Microsoft.EntityFrameworkCore;
+using ProjectManagment.Domain.Entities;
+
+public class ProjectDbContext : DbContext
+{
+    public ProjectDbContext(DbContextOptions<ProjectDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Project> projects { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<Project>().ToTable(nameof(Project)).HasKey(p => p.Id);
+        modelBuilder.Entity<Project>().Property(p => p.ProjectName).HasMaxLength(150);
+
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
