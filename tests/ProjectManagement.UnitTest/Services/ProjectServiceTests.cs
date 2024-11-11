9261562e0eec9e46817749fb96fca3810f607615
jalal-sagaie <jalal.sagaie@gmail.com>
2024-11-11 23:57:34 +0100

    Implemented getAll unit tests

---
+++ tests/ProjectManagement.UnitTest/Services/ProjectServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using ProjectManagment.Business.Project;
+
+namespace ProjectManagement.UnitTest.Services;
+
+public class ProjectServiceTests
+{
+    private readonly TestTools _testTools;
+    public ProjectServiceTests()
+    {
+        _testTools = new TestTools();
+        _testTools.Initialize(nameof(ProjectServiceTests));
+    }
+
+    #region GetAll
+    [Fact]
+    public async Task GetAll_ShouldHasData()
+    {
+        ProjectService projectService = new ProjectService(_testTools.ProjectMemoryDbContext);
+
+        var response = await projectService.GetAllAsync(CancellationToken.None);
+        Assert.True(response.Any());
+    }
+    #endregion
+
+
+}
---
+++ tests/ProjectManagement.UnitTest/TestTools.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectManagment.Domain.DbContext;
+using ProjectManagment.Domain.Entities;
+
+namespace ProjectManagement.UnitTest;
+
+public class TestTools
+{
+
+    public ProjectDbContext ProjectMemoryDbContext;
+
+    /// <summary>
+    /// Initialization
+    /// </summary>
+    public void Initialize(string testClassName)
+    {
+        var dbContextOptionsBuilder = new DbContextOptionsBuilder<ProjectDbContext>();
+
+        dbContextOptionsBuilder.UseInMemoryDatabase($"ProjectMemoryDbContext_{testClassName}");
+        DbContextOptions<ProjectDbContext>? contextOptions = dbContextOptionsBuilder.Options;
+        ProjectMemoryDbContext = new ProjectDbContext(contextOptions);
+        SeedData();
+    }
+
+    /// <summary>
+    /// Initializing new data
+    /// </summary>
+    public void SeedData()
+    {
+        List<Project> projectList = new List<Project>();
+        if (!ProjectMemoryDbContext.projects.Any())
+        {
+            // Add new project
+
+            for (int i = 1; i <= 3; i++)
+            {
+                projectList.Add(new Project
+                {
+                    ProjectName = $"Test{i}",
+                    CreatedDate = DateTime.Now,
+                    ProjectVersion = i,
+                    StartDate = DateTime.Now,
+                    Description = $"Test{i}"
+                });
+            }
+
+            ProjectMemoryDbContext.projects.AddRange(projectList);
+        }
+        ProjectMemoryDbContext.SaveChangesAsync();
+
+    }
+
+}
